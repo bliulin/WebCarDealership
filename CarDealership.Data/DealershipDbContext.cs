4be68f60e8a1164b789f19fe909929d097b38a1c
bliulin <bliulinx@gmail.com>
2022-03-16 19:00:47 +0200

    Add project files.

---
+++ CarDealership.Data/DealershipDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CarDealership.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebCarDealership
+{
+    public class DealershipDbContext : DbContext
+    {
+        public DealershipDbContext(DbContextOptions<DealershipDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.LogTo(Console.Write);
+        }
+
+        public DbSet<CarOffer> CarOffers { get; set; }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
